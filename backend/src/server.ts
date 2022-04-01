e6fe4231b289cd291161ee81eabb52c22f344068
Vitor Carvalho <vitor.carvalho@opinionbox.com>
2022-04-01 15:51:16 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const SERVER_PORT = 6651;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ id: 1, nome: "Pesquisa 123" });
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`SERVER START AT PORT ${SERVER_PORT}`);
+});
