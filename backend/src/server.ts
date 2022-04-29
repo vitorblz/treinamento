2d992aadb684164f1b6bb008121d00e0b669f5a8
Vitor Carvalho <vitor.carvalho@opinionbox.com>
2022-04-29 12:44:48 -0300

    feat: aula 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 const SERVER_PORT = 6651;
 
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ id: 1, nome: "Pesquisa 123" });
+app.get('/', (req, res) => {
+    res.json({ id: 1, nome: 'Pesquisa 123' });
 });
 
 app.listen(SERVER_PORT, () => {
-  console.log(`SERVER START AT PORT ${SERVER_PORT}`);
+    console.log(`SERVER START AT PORT ${SERVER_PORT}`);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 123
         </a>
       </header>
     </div>
